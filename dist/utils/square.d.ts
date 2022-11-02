31a3d3dfa5e01d99de9adcd18529793dbc142aa5
alex <alexlhliang@gmail.com>
2022-11-02 11:57:56 -0700

    added dist to gitignore

--- dist/utils/square.d.ts
+++
@@ -1,3 +0,0 @@
-import { Square, SquareIdx } from 'src/types/types';
-export declare function convertSquareToIdx(square: Square): SquareIdx;
-export declare function convertIdxToSquare(idx: SquareIdx): Square;
